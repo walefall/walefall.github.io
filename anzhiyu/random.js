63805d45b86031ddaad10d807bb0066327e5c6b7
walefall <1255749769@qq.com>
2024-09-08 22:18:26 +0800

    Site updated: 2024-09-08 22:18:25

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/09/05/GraphSAGE/","2024/09/05/GCN/","2024/09/05/hello-world/","2024/09/05/注意力机制/","2024/09/05/生成式时间序列/","2024/09/05/TGCN/","2024/09/04/页面手册/","2024/09/05/GAT/"];function toRandomPost(){
+var posts=["2024/09/05/GAT/","2024/09/05/GCN/","2024/09/05/TGCN/","2024/09/05/GraphSAGE/","2024/09/05/hello-world/","2024/09/05/注意力机制/","2024/09/04/页面手册/","2024/09/05/生成式时间序列/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
